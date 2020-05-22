bd9c808dfef710d817dab7ffb60a1ca9643c92f2
Randall Lough <randallmlough@gmail.com>
2020-05-22 14:02:32 -0700

    init

---
+++ convert_test.go
@@ -0,0 +1,23 @@
+// testing really need more improvements!
+// Please add more tests if you use this
+
+package govert
+
+import "testing"
+
+func TestShortType(t *testing.T) {
+	testShortType(t, "gitlab.com/product/app/backend/graphql_models.FlowWhere", "FlowWhere")
+	testShortType(t, "*gitlab.com/product/app/backend/graphql_models.FlowWhere", "*FlowWhere")
+	testShortType(t, "*github.com/web-ridge/go-utils/boilergql/boilergql.GeoPoint", "*GeoPoint")
+	testShortType(t, "github.com/web-ridge/go-utils/boilergql/boilergql.GeoPoint", "GeoPoint")
+	testShortType(t, "*string", "*string")
+	testShortType(t, "string", "string")
+	testShortType(t, "*time.Time", "*time.Time")
+}
+
+func testShortType(t *testing.T, input, output string) {
+	result := getShortType(input)
+	if result != output {
+		t.Errorf("%v should result in %v but did result in %v", input, output, result)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/randallmlough/govert
+
+go 1.14
+
+require (
+	github.com/99designs/gqlgen v0.11.3
+	github.com/iancoleman/strcase v0.0.0-20191112232945-16388991a334
+	github.com/pkg/errors v0.9.1
+	github.com/vektah/gqlparser/v2 v2.0.1
+	github.com/web-ridge/go-pluralize v0.1.5
+	golang.org/x/tools v0.0.0-20200507205054-480da3ebd79c
+)
